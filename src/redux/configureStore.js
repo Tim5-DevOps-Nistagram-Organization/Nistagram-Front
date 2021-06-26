9b561626a6e3637b6f44a7f9e28336f7983a28ce
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-26 17:10:41 +0200

    Update configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,6 +4,7 @@ import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
+  const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   return createStore(
     rootReducer,
